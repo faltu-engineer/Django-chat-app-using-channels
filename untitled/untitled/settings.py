27e472fe1c543bef262c40982189a010ba4fb939
Rahul Rajput <raj.rahul585@gmail.com>
2020-11-18 21:08:38 +0530

    secret keys removed

--- untitled/untitled/settings.py
+++ untitled/untitled/settings.py
@@ -19,9 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ix#9$%#gga)f9#buqk-#51oh2lllel0$i3&h7qey@24*l81b-_'
-
+#secret key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
